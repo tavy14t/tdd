a24b5fc3d911a2be807cf6af56adfd31a72d5e1a
Ilie Octavian <tavy14t@gmail.com>
2017-03-20 13:40:30 +0200

    tdd - isLeapYear

--- src/MathService.java
+++ src/MathService.java
@@ -1,5 +1,11 @@
 public class MathService {
     public static boolean isLeapYear(int year) {
-        return year % 4 == 0;
+        if(year < 0)
+            return false;
+        else if(year % 4 == 0 && year % 100 != 0)
+            return true;
+        else if(year % 400 == 0)
+            return true;
+        return false;
     }
 }
--- src/MathServiceTest.java
+++ src/MathServiceTest.java
@@ -4,10 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MathServiceTest {
-    // a year is a leap year if is divisible by 4
-    // but, years divisible by 100 are not leap years
-    // except years divisible by 400
-
     @Test
     public void leap_year_should_be_divisible_by_four(){
         assertTrue(MathService.isLeapYear(2016));
@@ -17,4 +13,29 @@ public class MathServiceTest {
     public void non_leap_year_should_not_be_divisible_by_four(){
         assertFalse(MathService.isLeapYear(3));
     }
+
+    @Test
+    public void leap_year_should_be_divisible_by_four_and_not_divisible_by_100(){
+        assertTrue(MathService.isLeapYear(2004));
+        assertFalse(MathService.isLeapYear(1800));
+    }
+
+    @Test
+    public void leap_year_should_be_divisible_by_400(){
+        assertTrue(MathService.isLeapYear(2000));
+    }
+
+    @Test
+    public void leap_year_must_be_positive(){
+        assertFalse(MathService.isLeapYear(-50));
+    }
+
+    @Test
+    public void multiple_tests(){
+        assertTrue(MathService.isLeapYear(2000));
+        assertTrue(MathService.isLeapYear(1904));
+        assertFalse(MathService.isLeapYear(1900));
+        assertFalse(MathService.isLeapYear(1901));
+    }
 }
+
